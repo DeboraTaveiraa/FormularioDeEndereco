4b91c9048a64787447ecf2386fe1656b06164f9a
Débora Taveira <deborataveirapvh@gmail.com>
2021-04-13 11:17:07 -0400

    Imprementação da etapa endereço do formulário

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-# FormularioDeEndereco
-Formulário de endereço com preenchimento automático através da inserção do CEP. (Consumo da API ViaCEP) (Vue CLI) (Axios)
+# Formulário baseado em entregas de produtos
+
+Requisição http realizada na API do [ViaCEP](https://viacep.com.br/) para retornar, através do CEP informado: rua, bairro, cidade e estado.
+
+![](img-form.png)
+
+## O que foi usado:
+[Vue CLI](https://cli.vuejs.org/) (Vue.js)
+
+[Axios](https://github.com/axios/axios)
+
+[SweetAlert2 for Vue.js](https://www.npmjs.com/package/vue-sweetalert2)
+
+
+## Como utilizar esse projeto:
+1) Faça a clonagem desse repositório em sua máquina.
+Para mais informações sobre essa etapa, [clique aqui](https://docs.github.com/pt/github/creating-cloning-and-archiving-repositories/cloning-a-repository).
+
+2) Abra o repositório clonado via terminal.
+
+3) Dentro do repositório clonado, execute:
+> npm install 
+
+ou
+> yarn
+
+Isso fará com que seja instalado todos as bibliotecas necessárias para executar o projeto.
+
+4) Execute o projeto pelo terminal com o comando:
+> yarn serve
+
+<br>
+
+**Me acompanhe nas redes sociais:**
+
+[![Linkedin Badge](https://img.shields.io/badge/-Débora%20Taveira-blue?style=flat-square&logo=Linkedin&logoColor=white&link=https://www.linkedin.com/in/débora-taveira/)](https://www.linkedin.com/in/débora-taveira/) [![Instagram Badge](https://img.shields.io/badge/-Débora%20Taveira-df3264?style=flat-square&logo=Instagram&logoColor=white&link=https://www.instagram.com/deborataveiracode/)](https://www.instagram.com/deborataveiracode/) [![Gmail Badge](https://img.shields.io/badge/-deborataveirapvh@gmail.com-red?style=flat-square&logo=Gmail&logoColor=white&link=mailto:deborataveirapvh@gmail.com)](mailto:deborataveirapvh@gmail.com) 
---
+++ src/components/Header.vue
@@ -0,0 +1,170 @@
+<template>
+   <header>
+        <div class="animation">
+            <i class="fas fa-truck"></i>
+        </div>
+        <h3>Confirmação do pedido</h3>
+        <ul>
+            <li class="active" data-step="1">Carrinho</li>
+            <li class="active" data-step="2">Forma de pagamento</li>
+            <li class="active" data-step="3">Endereço</li>
+            <li data-step="4">Finalizar</li>
+        </ul>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'Header'
+}
+</script>
+
+<style>
+header {
+    margin-top: 40px;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+}
+header div.animation {
+    background: linear-gradient(180deg, rgba(104,6,252,1) 0%, rgba(246,40,231,1) 100%);
+    width: 150px;
+    height: 150px;
+    border-radius: 50%;
+    display: flex;
+    justify-content: center;
+}
+
+header div i {
+    font-size: 4em;
+    margin-top: 40px;
+    color: #fff;
+    animation-name: carrinho;
+    animation-duration: 1s;
+    animation-iteration-count: infinite;
+    position: absolute;
+    animation-direction: alternate;
+    animation-timing-function: linear;
+}
+header h3 {
+    margin-top: 20px;
+    color: #f628e7;
+    font-weight: 500;
+}
+
+ul {
+  margin-top: 70px;
+  list-style: none;
+  display: flex;
+  width: 650px;
+  padding: 0;
+}
+ul li {
+  position: relative;
+  color: gray;
+  width: 25%;
+  text-align: center;
+}
+ul li::before {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  content: attr(data-step);
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  background: #f2f2f2;
+  position: absolute;
+  top: -50px;
+  left: calc(50% - 20px);
+  z-index: 99;
+}
+ul li::after {
+  content: "";
+  width: 100%;
+  height: 5px;
+  background: #f2f2f2;
+  position: absolute;
+  top: -35px;
+  left: -50%;
+}
+ul li:first-child::after {
+  width: 0;
+}
+ul li.active::before {
+  background: #6806fc;
+  color: #fff;
+}
+ul li.active::after {
+  background: #6806fc;
+}
+ul li.active {
+  color: #6806fc;
+}
+
+@keyframes carrinho {
+    50% {
+        margin-top: 30px;
+       
+        }
+    100% {
+        padding-bottom: 15px
+    }
+}
+
+@keyframes carrinhoMobile {
+    50% {
+        margin-top: 40px;
+       
+        }
+    100% {
+        padding-bottom: 10px
+    }
+}
+
+@media (max-width: 767px) {
+    header div {
+        width: 100px;
+        height: 100px;
+    }
+    header div i {
+        font-size: 3em;
+        margin-top: 50px;
+        animation-name: carrinhoMobile;
+        animation-duration: 1s;
+        animation-iteration-count: infinite;
+        position: absolute;
+        animation-direction: alternate;
+        animation-timing-function: linear;
+    }
+    ul {
+        width: 70%;
+    }
+}
+
+@media (max-width: 600px) {
+    header div.animation {
+        width: 70px;
+        height: 70px;
+    }
+    div.animation i {
+        margin-top: 25px;
+        margin-bottom: 30px;
+        font-size: 1em;
+    }
+  header h3 {
+    font-size: 1.1em;
+  }
+
+  ul {
+      width: 80%;
+  }
+
+  ul li {
+      font-size: 0.8em;
+      line-height: 12px;
+  }
+}
+</style>
